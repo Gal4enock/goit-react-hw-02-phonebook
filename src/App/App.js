bd49638714dbbd8caefa5890ad2bd2e3ace6a9f6
Galyna Khyzhynska <gal4enock86@gmail.com>
2020-11-26 17:49:01 +0200

    добавляет стили

--- src/App/App.js
+++ src/App/App.js
@@ -3,31 +3,37 @@ import { v4 as uuidv4 } from 'uuid';
 import Contacts from '../Contacts/Contacts';
 import ContactForm from '../ContactForm/ContactForm';
 import Filter from '../FilterContacts/FilterContacts'
-import './App.module.css';
 
 class App extends Component {
   state = {
     contacts: [],
-    filter: '',
-    name: '',
-    number: ''
+    filter: ''
   }
 
   handleChange = (e) => {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState(
-      {
-        contacts: [...this.state.contacts, { name: this.state.name, id: uuidv4(), number: this.state.number }],
-        name: '',
-        number: ''
+  addContacts = (name, number) => {
+    const contact = {
+      name,
+      id: uuidv4(),
+      number
+    }
+    console.log(contact.name);
+    const doubleName = this.state.contacts.find(el => el.name === contact.name);
+    if (doubleName) {
+      alert(`${contact.name} is already in contacts`);
+      return
+    }
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contact]
       }
-    )
+    })
   }
+  
+  
   handleDelete = nameId => {
     this.setState(prevState => {
       return {
@@ -35,23 +41,20 @@ class App extends Component {
       }
     })
   }
-  changeFilter = (filter) => {
-    this.setState({filter})
-  }
-    handleFilter = () => {
+   handleFilter = () => {
       const { contacts, filter } = this.state;
-      return (contacts.filter((contact => contact.name.toLowerCase().includes(filter.toLowerCase()) )))
+      return (contacts.length ? contacts.filter((contact => contact.name.toLowerCase().includes(filter.toLowerCase()) )) : '')
       
     }
     
   render() {
-    const { name, number, filter } = this.state;
+    const {filter} = this.state;
     return (
       <div>
         <h1>Phonebook</h1>
-       <ContactForm onSubmit={this.handleSubmit} value={name} number={number} onChange={this.handleChange}/>
+        <ContactForm toAddContacts={this.addContacts}/>
         <h2>Contacts</h2>
-        <Filter value={filter} toFilter={this.changeFilter} />
+        <Filter value={filter} toFilter={this.handleChange} />
         <Contacts contacts={this.handleFilter()} onDelete={this.handleDelete}/>
       </div>
     )
--- src/ContactForm/ContactForm.jsx
+++ src/ContactForm/ContactForm.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import style from './ContactForm.module.css'
+import PropTypes from 'prop-types';
 
-const ContactForm = function ({onSubmit, value, onChange, number}) {
-  return (
-  <form onSubmit={onSubmit}>
-    <label className={style.lable}>
-      Name
-      <input className={style.input} name="name" type="text" value={value} onChange={onChange} />
-    </label>
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.toAddContacts(this.state.name, this.state.number);
+    this.setState({
+      name: '',
+      number: ''
+    })
+   }
+  render() {
+    return ( <form className={style.form} onSubmit={this.handleSubmit}>
       <label className={style.lable}>
-      Number
-      <input className={style.input} name="number" type="phone" value={number} onChange={onChange} />
-      <button type="submit">Add contact</button>
-    </label>
-  </form>
-  )
+        Name
+      <input className={style.input} name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+      </label>
+      <label className={style.lable}>
+        Number
+      <input className={style.input} name="number" type="phone" value={this.state.number} onChange={this.handleChange} />
+        <button type="submit">Add contact</button>
+      </label>
+    </form>)
+  }
 }
+
 export default ContactForm;
